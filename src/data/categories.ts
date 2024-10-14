235f83659adc3b44ab21a7176e14897c65ecfacc
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-10-14 19:19:50 +0300

    feat(categories): update categories in the blog posts and images

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -8,16 +8,14 @@ import ui from '../assets/images/categories/ui-libraries.jpg';
 import azure from '../assets/images/categories/azure.jpg';
 import o365 from '../assets/images/categories/o365.jpg';
 
-
-
 export const CATEGORIES = [
-	{ name: 'Database', image: database },
-	{ name: 'Design Patterns', image: designPatterns },
-	{ name: 'ASP.NET Core', image: aspnetcore },
-	{ name: '.NET', image: dotnetImage },
-	{ name: 'Blazor', image: blazor },
-	{ name: 'Csharp', image: csharp },
-	{ name: 'UI', image: ui },
-	{ name: 'Azure', image: azure },
-	{ name: 'O365', image: o365 }
+	{ name: 'AccountingTechnician', description: 'Accounting technicians handle day-to-day money and finance tasks for all types of business.', image: database },
+	{ name: 'AdminAssistant', description: 'Admin assistants give support to businesses by organising meetings, typing documents and updating computer records.', image: designPatterns },
+	{ name: 'ArtsAdministrator', description: 'Arts administrators help organise exhibitions, manage staff and look after buildings like museums.', image: aspnetcore },
+	{ name: 'AssistantImmigrationOfficer', description: 'Assistant immigration officers check that people have the right to visit or stay in the UK.', image: dotnetImage },
+	{ name: 'Auditor', description: 'Internal and external auditors check organisations financial records and procedures to make sure they are accurate and efficient.', image: blazor },
+	{ name: 'BidWriter', description: 'Bid writers prepare documents used to request contracts to provide services or apply for funding.', image: csharp },
+	{ name: 'BilingualSecretary', description: 'Bilingual secretaries provide administrative services in English and one or more foreign languages.', image: ui },
+	{ name: 'Bookkeeper', description: 'Bookkeepers keep financial records up to date and help prepare accounts.', image: azure },
+	{ name: 'BorderForceOfficer', description: 'Border Force officers protect UK border entry points like ports and airports, by enforcing immigration and customs regulations.', image: o365 }
 ] as const;
