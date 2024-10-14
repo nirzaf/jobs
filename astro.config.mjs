918b4570f086f47268265198865fb37eff7830ae
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-10-14 18:37:45 +0300

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,34 @@
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import { remarkReadingTime } from './src/utils/readTime.ts'
+
+export default defineConfig({
+	site: 'https://dotnetevangelist.com',
+	trailingSlash: 'never',
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+		drafts: true,
+		shikiConfig: {
+			theme: 'material-theme-palenight',
+			wrap: true
+		}
+	},
+	integrations: [
+		mdx({
+			syntaxHighlight: 'shiki',
+			shikiConfig: {
+				experimentalThemes: {
+					light: 'vitesse-light',
+					dark: 'material-theme-palenight',
+				},
+				wrap: true
+			},
+			drafts: true
+		}),
+		sitemap(),
+		tailwind()
+	],
+	outDir: './dist'
+})
---
+++ src/data/site.config.ts
@@ -0,0 +1,19 @@
+interface SiteConfig {
+	author: string
+	title: string
+	description: string
+	lang: string
+	ogLocale: string
+	shareMessage: string
+	paginationSize: number
+}
+
+export const siteConfig: SiteConfig = {
+	author: 'M.F.M Fazrin', // Site author
+	title: 'DotNet Blogs by Dotnet Evangelist ', // Site title.
+	description: 'Explore the latest insights, tutorials, and best practices in .NET development from a passionate Dotnet Evangelist. Stay up-to-date with cutting-edge technologies and enhance your skills in C#, ASP.NET, and more.', // Description to display in the meta tags
+	lang: 'en-GB',
+	ogLocale: 'en_GB',
+	shareMessage: 'Share this post', // Message to share a post on social media
+	paginationSize: 9 // Number of posts per page
+}
